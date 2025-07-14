be448d16949f87dd453b11af80839dd1d78842bc
Syakirah Edjans <drshark02@gmail.com>
2025-07-14 17:32:35 +0800

    gadv project day 1

---
+++ Assets/Scripts/PlaySoundOnTrigger2D.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTrigger2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource soundEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            soundEffect.Play();
+        }
+    }
+}
